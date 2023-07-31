093b8d774e69c730901522b47d4edb27d8dea0e8
K <kelseydrhoda@gmail.com>
2023-07-31 10:31:08 -0600

    Adds Svelte examples including all build tools, maintains naming consistency

---
+++ examples/svelte-webpack-example/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let name: string;
+	import { Sorter } from "wasm_quicksort_example";
+
+	let v = [3, 2, 1];
+	Sorter.initialize().then((sorter) => {
+		let n = sorter.sort(v);
+		alert(`Sorted array: ${JSON.stringify(n)}`);
+	});
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
