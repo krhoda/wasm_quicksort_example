20bda31574899663626ae3767f20075589781563
K <kelseydrhoda@gmail.com>
2023-08-01 11:25:09 -0600

    Fixed incorrectly routed path

--- wasm_quicksort_example/src/index.ts
+++ wasm_quicksort_example/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./index_core.js";
-import wasm_mergesort_example_wasm from "./pkg/wasm_mergesort_example_bg.wasm";
-import {setWasmInit} from "./mergesort_wrapper.js"
+import wasm_quicksort_example_wasm from "./pkg/wasm_quicksort_example_bg.wasm";
+import {setWasmInit} from "./quicksort_wrapper.js"
 
 // @ts-ignore
 setWasmInit(() => wasm_quicksort_example_wasm());
